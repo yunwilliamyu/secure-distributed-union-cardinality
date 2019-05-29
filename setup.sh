a5142377364af8745be703440782ef05b149c4c9
Yun William Yu <contact@yunwilliamyu.net>
2019-05-29 05:20:15 -0400

    Changed serialized filename

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo Downloading precomputed log tables for ElGamal Count MPC
-# wget -c TAB-table.pickle
+# wget -c elgamal-billion-table.npy
 
 echo Precomputing some initial patient lists for simulation
 seq 1 100000 > simulation/10_5.txt
